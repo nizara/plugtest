ce8675cea4490c0014462f32db7252b41c2b3ec5
n.ajroud <n.ajroud@AjroudN-Dell.sofrecom-tunisie.com>
2015-09-15 14:25:26 +0100

    initial commit

---
+++ maventest1/src/main/java/nizar/trainings/plug/maventest1/App.java
@@ -0,0 +1,13 @@
+package nizar.trainings.plug.maventest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
